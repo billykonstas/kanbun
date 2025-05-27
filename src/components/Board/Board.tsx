cffa1e6aebe7054ce49240a776abedd060e0235e
billykonstas <billykonstas@outlook.com>
2025-05-27 22:01:54 +0300

    not used icon

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import Column from "./Column.tsx";
 import { useBoard } from "@/contexts/BoardContext.tsx";
 import { Board as BoardType } from "@/types/board.ts";
-import { BetweenVerticalStart } from "lucide-react";
 
 const Board: React.FC<{ boardId: BoardType["id"] }> = ({ boardId }) => {
   const { currentBoard, selectBoard } = useBoard();
@@ -22,7 +21,6 @@ const Board: React.FC<{ boardId: BoardType["id"] }> = ({ boardId }) => {
     <div className="h-full flex-col flex w-full">
       <div className="bg-brown text-beige flex content-center z-20 p-4 ml-2 mb-3 rounded-lg items-center">
         <span className="text-xl font-bold">{currentBoard.title}</span>
-        <BetweenVerticalStart width="18" height="18" />
       </div>
       <div className="flex space-x-4 flex-1 p-2 w-full h-full overflow-x-auto snap-x snap-mandatory rounded-xl">
         {currentBoard.columns.map((column) => (
